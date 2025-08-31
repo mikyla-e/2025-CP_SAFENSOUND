07c5a07738e580e1c9027ee9bb5ced523238d678
noex08 <mikylam.escudero@gmail.com>
2025-09-01 00:23:13 +0800

    arranged libraries and prepared gpio setup
    12:22am 09/01/25
    
    prepared libraries, gpio setup, functions
    added reset.py

--- iot/rpi_main.py
+++ iot/rpi_main.py
@@ -29,21 +29,54 @@ from sklearn.model_selection import train_test_split
 import RPi.GPIO as GPIO
 
 # GPIO setup ---------------------------------------
+# GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
+
+light_pin_1 =
+light_pin_2 = 
+light_pin_3 =
+
+buzzer_pin =
+
+reset_pin =
+
+GPIO.setup([light_pin_1, light_pin_2, light_pin_3, buzzer_pin], GPIO.OUT)
+GPIO.setup(reset_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 # Load the pre-trained model
 # model = joblib.load("model.joblib")
 
 
 # functions -----------------------------------------
+audio_duration = 5 #seconds
+sample_rate = 16000
+ml_model_path = ""
+
+
 def listen_and_classify():
-    # Record audio
+    # record audio
+
+    emergency = 0
+    emergency_detected = False
+
+    # proceeding 2-4 audios after the first emergency audio detected must also be detected as emergency to trigger alarm
+    if emergency_detected:
+        emergency += 1
+        trigger_alarm()
+    else:
+        emergency = 0
 
 def extract_features():
     # extract features from audio
+    features =
+
+    
+    return features
 
 def trigger_alarm():
     # trigger alarm if emergency was detected
 
-
+    
 
 try:
     while True:
