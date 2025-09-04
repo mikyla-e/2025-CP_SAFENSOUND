10a4268e6227274b46389ed53d086ecad7ce6b00
noex08 <mikylam.escudero@gmail.com>
2025-09-04 15:59:53 +0800

    created a new clean folder for datasets and updated random forest model
    3:58pm 09/04/25
    
    dataset folder with old files can be found in 'old datasets' folder

--- iot/rpi_main.py
+++ iot/rpi_main.py
@@ -1,11 +1,13 @@
 # --- general ---
 import os
 import time
+from time import sleep
 import datetime
 
 # --- networking ---
 import socket
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqtt     
+import bluetooth
 
 # --- audio processing ---
 import joblib
@@ -30,41 +32,68 @@ import RPi.GPIO as GPIO
 
 # GPIO setup ---------------------------------------
 # GPIO.setmode(GPIO.BOARD)
-GPIO.setmode(GPIO.BCM)
+# GPIO.setmode(GPIO.BCM)
 
-light_pin_1 =
-light_pin_2 = 
-light_pin_3 =
+# light_pin_1 =
+# light_pin_2 = 
+# light_pin_3 =
 
-buzzer_pin =
+# buzzer_pin =
 
-reset_pin =
+# reset_pin =
 
-GPIO.setup([light_pin_1, light_pin_2, light_pin_3, buzzer_pin], GPIO.OUT)
-GPIO.setup(reset_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# GPIO.setup([light_pin_1, light_pin_2, light_pin_3, buzzer_pin], GPIO.OUT)
+# GPIO.setup(reset_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# Load the pre-trained model
+# load model vvv
 # model = joblib.load("model.joblib")
 
 
 # functions -----------------------------------------
-audio_duration = 5 #seconds
-sample_rate = 16000
 ml_model_path = ""
 
+audio_wav = None
+audio_duration = 5 #seconds
+sample_rate = 16000
 
-def listen_and_classify():
-    # record audio
 
-    emergency = 0
+def connection():
+    
+
+
+
+def get_audio():
+    # get audio from microphone
+    audio_wav = None
+    date_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    try:
+        print(f"Recording audio... at {date_time}")
+        audio = sd.rec(int(audio_duration * sample_rate), samplerate=sample_rate, channels=1)
+        sd.wait()
+
+        audio_wav = f"recording_{date_time}.wav"
+        lb.output.write_wav(audio_wav, audio, sample_rate)
+        print(f"Audio recorded and saved as {audio_wav}")
+
+    except Exception as e:
+        print("Audio recording failed:", e)
+
+
+    return audio_wav
+
+
+def inference():
+
+    emergency_count = 0
     emergency_detected = False
 
-    # proceeding 2-4 audios after the first emergency audio detected must also be detected as emergency to trigger alarm
+    # proceeding 2-4 audios after the first emergency audio detected must also be detected as emergency to trigger alarm (?)
     if emergency_detected:
-        emergency += 1
+        emergency_count += 1
         trigger_alarm()
     else:
-        emergency = 0
+        emergency_count = 0
 
 def extract_features():
     # extract features from audio
@@ -76,12 +105,13 @@ def extract_features():
 def trigger_alarm():
     # trigger alarm if emergency was detected
 
+
     
 
 try:
     while True:
-        listen_and_classify()
-        time.sleep(1)  # Small delay before next recording
+        get_audio()
+        time.sleep(1)
 except KeyboardInterrupt:
     print("\n[INFO] Exiting gracefully...")
     GPIO.cleanup()
