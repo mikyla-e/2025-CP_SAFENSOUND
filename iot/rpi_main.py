5d04c01672d059170da8827fec1bb0c3e4765407
noex08 <mikylam.escudero@gmail.com>
2026-02-28 16:39:24 +0800

    Update rpi_main.py

--- iot/rpi_main.py
+++ iot/rpi_main.py
@@ -920,11 +920,11 @@ async def send_alert_rpi(device_add, room_id, action=None):
         try:
             await asyncio.sleep(1)
             match device_id:
-                case 1:
+                case 5:
                     led1_active = True
                     led_pin_1.blink(on_time=0.5, off_time=0.5)
                     print("LED 1 activated.")
-                case 5:
+                case 1:
                     led2_active = True
                     led_pin_2.blink(on_time=0.5, off_time=0.5)
                     print("LED 2 activated.")
@@ -970,11 +970,11 @@ async def send_reset_rpi(device_add, action=None):
     if "Alert Acknowledged" in action:
         try:
             match device_id:
-                case 1:
+                case 5:
                     led1_active = False
                     led_pin_1.off()
                     print("LED 1 deactivated.")
-                case 5:
+                case 1:
                     led2_active = False
                     led_pin_2.off()
                     print("LED 2 deactivated.")
